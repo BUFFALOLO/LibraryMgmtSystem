c83161d79ef057b618ad5e7d2d26a53f072b9bd8
Lauren Farrell <150389282+BUFFALOLO@users.noreply.github.com>
2025-02-03 18:30:39 -0500

    Delete AuthorClass.py

--- AuthorClass.py
+++
@@ -1,7 +0,0 @@
-class Author:
-    def __init__(self, name, bio):
-        self.__name = name
-        self.__bio = bio
-
-    def display_details(self):
-        return f"Author Name: {self.__name}, Biography: {self.__bio}"
